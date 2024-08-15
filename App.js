012d77ff96a3a82aa5aa0d13c3c3f88fef8471a4
parthgarg351 <parthgarg351@gmail.com>
2024-08-15 19:59:35 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,30 @@
+const heading2 = React.createElement("h1", { id: "heading", xyz: "hello" },//these are called props
+     "Hello World From React"); //this is called children   
+     //heading2 is object created by react
+     const root2 = ReactDOM.createRoot(document.getElementById("root"));
+     root2.render(heading2); //render will convert object html tag
+     
+     //--------------nested tags--------------
+     /*
+     <div id ="parent">
+        <div id ="child">
+            <h1>Hi I am an H1 Tag.</h1>
+            <h2>Hi I am A H2 Tag.</h2>
+        </div>
+        <div id ="child2">
+            <h1>Hi I am an H1 Tag.</h1>
+            <h2>Hi I am A H2 Tag.</h2>
+        </div>
+     </div>
+     */
+    const heading3 = React.createElement("div",{id:"parent"},[
+        React.createElement("div",{id:"child"},[
+            React.createElement("h1",{},"Hi I am a H1 tag."),
+            React.createElement("h2",{},"hi I am a H2 tag")]),
+            React.createElement("div",{id:"child"},[
+            React.createElement("h1",{},"Hi I am a H1 tag."),
+            React.createElement("h2",{},"hi I am a H2 tag")])
+    ]
+        );
+    
+    root2.render(heading3);
---
+++ README.md
@@ -0,0 +1,7 @@
+How to set up ssh in git in windos
+
+NPN does not stands for Node Package Manager but works as a Node Package Manager.
+
+package.json is a configuration for NPN.
+
+Our project depends upon packages and packages depends upon a lot of dependencies and all this is managed by NPN.  What is the version of that package NPN will take care of it.
